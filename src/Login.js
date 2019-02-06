893abb93553e753d7a693439be80d2212a8bc02b
Liang200 <827865721@qq.com>
2019-02-06 00:43:15 -0500

    create URL constants and login request

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Form, Icon, Input, Button } from 'antd';
+import { Form, Icon, Input, Button , message } from 'antd';
+import {API_ROOT} from "./constants"
 
 
 class NormalLoginForm extends React.Component {
@@ -8,6 +9,23 @@ class NormalLoginForm extends React.Component {
         this.props.form.validateFields((err, values) => {
             if (!err) {
                 console.log('Received values of form: ', values);
+                fetch(`${API_ROOT}/login` , {
+                    method : 'POST',
+                    body: JSON.stringify({
+                        username: values.username,
+                        password: values.password,
+                    }),
+                }).then((response) => {
+                    if(response.ok) {
+                        return response.text();
+                    }
+                    throw new Error(response.statusText);
+                }).then((data) => {
+                    message.success('Login Success!');
+                }).catch((e) => {
+                    console.log(e);
+                    message.error('Login fail');
+                })
             }
         });
     }
@@ -17,7 +35,7 @@ class NormalLoginForm extends React.Component {
         return (
             <Form onSubmit={this.handleSubmit} className="login-form">
                 <Form.Item>
-                    {getFieldDecorator('userName', {
+                    {getFieldDecorator('username', {
                         rules: [{ required: true, message: 'Please input your username!' }],
                     })(
                         <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
